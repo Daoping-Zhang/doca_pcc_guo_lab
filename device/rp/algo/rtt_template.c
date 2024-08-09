0ef04583e92245dbbb67c168feb50d1e971cc267
Daoping-Zhang <ttspz1019@gmail.com>
2024-08-09 21:00:25 +0800

    2

--- device/rp/algo/rtt_template.c
+++ device/rp/algo/rtt_template.c
@@ -237,11 +237,23 @@ static inline uint32_t new_rate_rtt(cc_ctxt_rtt_template_t *ccctx,
 
 	if( gradient_fixed <= 0)
 	{
-		ccctx->pro_rate += param[RTT_TEMPLATE_AI];
-		ccctx->pro_rate = ccctx->rx_rate <= ccctx->pro_rate ? ccctx->rx_rate : ccctx->pro_rate;
-		//ccctx->rx_rate = cur_rate;
+		if(rtt < param[RTT_TEMPLATE_BASE_RTT]*2)
+		{
+			ccctx->pro_rate += param[RTT_TEMPLATE_AI];
+			ccctx->pro_rate = ccctx->rx_rate <= ccctx->pro_rate ? ccctx->rx_rate : ccctx->pro_rate;
+			//ccctx->rx_rate = cur_rate;
 
-		cur_rate += param[RTT_TEMPLATE_AI];
+			cur_rate += param[RTT_TEMPLATE_AI];
+		}else
+		{
+			ccctx->con_rate = ccctx->rx_rate >= cur_rate ? ccctx->rx_rate : cur_rate;
+			ccctx->rx_rate = cur_rate;
+			//doca_pcc_dev_printf("%s, min_rtt: %d rtt: %d gradient_fixed: %ld  new_rtt_diff: %d pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, gradient_fixed, new_rtt_diff, ccctx->pro_rate, ccctx->con_rate, cur_rate);
+			cur_rate = (ccctx->pro_rate + cur_rate)/2;
+
+			ccctx->pro_rate = ccctx->pro_rate > param[RTT_TEMPLATE_AI] ? ccctx->pro_rate - param[RTT_TEMPLATE_AI] : 0;			
+		}
+
 
 		//doca_pcc_dev_printf("%s, up min_rtt: %d rtt: %d gradient_fixed: %u pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, gradient_fixed, ccctx->pro_rate, ccctx->con_rate, cur_rate);
 
@@ -265,7 +277,7 @@ static inline uint32_t new_rate_rtt(cc_ctxt_rtt_template_t *ccctx,
 
 	if(rtt_times>0)
 	{	
-		//doca_pcc_dev_printf("%s, min_rtt: %d rtt: %d gradient_fixed: %ld  new_rtt_diff: %d pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, gradient_fixed, new_rtt_diff, ccctx->pro_rate, ccctx->con_rate, cur_rate);
+		doca_pcc_dev_printf("%s, min_rtt: %d rtt: %d gradient_fixed: %ld  new_rtt_diff: %d pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, gradient_fixed, new_rtt_diff, ccctx->pro_rate, ccctx->con_rate, cur_rate);
 		//rtt_times--;
 	}
 
@@ -429,7 +441,7 @@ static inline void rtt_template_handle_roce_rtt(doca_pcc_dev_event_t *event,
 
 	if (unlikely(end_rtt < start_rtt)){
 		rtt += UINT32_MAX;
-		//doca_pcc_dev_printf("%s, min_rtt: %d rtt: %d pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, ccctx->pro_rate, ccctx->con_rate, cur_rate);
+		doca_pcc_dev_printf("%s, min_rtt: %d rtt: %d pro_rate: %u con_rate: %u cur_rate: %u \n", __func__,ccctx->min_rtt, rtt, ccctx->pro_rate, ccctx->con_rate, cur_rate);
 	}
 
 		
